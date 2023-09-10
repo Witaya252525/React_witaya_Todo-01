e5bb739caced85f0fe8b5c20708cbe279ec901ab
Witaya252525 <witaya.c@kkumail.com>
2023-09-10 21:28:19 +0700

    state

---
+++ src/App component.js
@@ -0,0 +1,11 @@
+function App() {
+  const name = "WITAYA CHAISON";
+  return (
+    <>
+      <h1> Hello world + {name} </h1>
+      <botton onClick ={() => alert ("Hellow react ")}>Click</botton>
+    </>
+  );
+}
+
+export default App;
